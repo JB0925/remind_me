0dce7e9c6ba3986d9a50e0b394859521176c238b
Jesse Brink <jbrink0925@gmail.com>
2021-04-02 22:26:23 -0400

    added dateutil and tweaked schedule_jobs to get it working

--- remind_me/schedule_jobs.py
+++ remind_me/schedule_jobs.py
@@ -1,7 +1,8 @@
-from sms import send
-from check_email import ReadEmail
+from remind_me.sms import send
+from remind_me.check_email import ReadEmail
 
 from apscheduler.schedulers.background import BlockingScheduler
+from dateutil.parser import parse
 
 sched = BlockingScheduler()
 
@@ -18,27 +19,30 @@ def multiply():
     print(4*4)
 
 
-def scheduled_job(func, msg, number, carrier):
-    func(msg, number, carrier)
+def scheduled_job(msg, number, carrier):
+    send(msg, number, carrier)
 
 
 def shutdown():
     sched.shutdown(wait=False)
 
 jobs = [
-    (send, 'kids dentist appt @ 12:30', 5409035731, 'Verizon', '04-01-2021 10:10'),
-    (send, 'go to sleep @ 11:30', 5409035731, 'verizon', '04-01-2021 10:11'),
-    (send, 'xyz', 2813308004, 'att', '04-01-10:12'),
-    (shutdown, '04-01-2021 10:13')
+    ('kids dentist appt @ 12:30', 5409035731, 'Verizon', '04-02-2021 20:55'),
+    ('go to sleep @ 11:30', 5409035731, 'verizon', '04-02-2021 20:56'),
+    ('xyz', 2813308004, 'att', '04-02-2021 20:57'),
+    (shutdown, '04-02-2021 20:58')
 ]
 
 
 for job in jobs:
-    if 'shutdown' in job[0]:
+    if len(job) == 2:
         job, run_date = job
+        run_date = parse(run_date)
         sched.add_job(shutdown, trigger='date', run_date=run_date)
-    job, msg, number, carrier, run_date = job     
-    sched.add_job(scheduled_job, args=(job,msg,number,carrier), trigger='date', run_date=run_date)
+    else:
+        msg, number, carrier, run_date = job
+        run_date = parse(run_date)
+        sched.add_job(scheduled_job, args=(msg,number,carrier), trigger='date', run_date=run_date)
     
 print(sched.get_jobs())
 sched.start()
