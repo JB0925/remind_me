66d9fd993e6ee396b6b8bf7188fd1e782977fd51
Jesse Brink <jbrink0925@gmail.com>
2021-04-07 15:09:21 -0400

    Updated files to reflect usage of the database for login and storage purposes

--- remind_me/services/user_service.py
+++ remind_me/services/user_service.py
@@ -2,7 +2,12 @@ from typing import Optional
 import hashlib
 import uuid
 
+from passlib.handlers.sha2_crypt import sha512_crypt as crypto
+
 from remind_me.data.user import User
+from remind_me.data import db_session
+
+
 
 
 def hash_password(password):
@@ -16,18 +21,46 @@ def check_password(hashed_password, entered_password):
 
 
 def create_account(name: str, email: str, password: str) -> User:
-    password = hash_password(password)
-    return User(name, email, password)
+    session = db_session.create_session()
+
+    try:
+        user = User()
+        user.name = name
+        user.email = email
+        user.hashed_password = crypto.hash(password, rounds=172_434)
+
+        session.add(user)
+        session.commit()
+        return user
+
+    finally:
+        session.close()
 
 
 def login_user(name: str, password: str) -> Optional[User]:
-    pw = hash_password(password)
-    if check_password(pw, password):
-        return User('test', 'test_email', 'pw')
-    return None
+    session = db_session.create_session()
+
+    try:
+        user = session.query(User).filter(User.name == name).first()
+        if not user:
+            return user
+        
+        if not crypto.verify(password, user.hashed_password):
+            return None
+        
+        return user
+    finally:
+        session.close()
 
 
 def make_job(event: str, number: str, carrier:str, date: str):
     return [(event, number, carrier, date)]
+
+
+def store_events(email, phone_number, carrier, event, date_and_time, user_id):
+    session = db_session.create_session()
+
+    try:
+        user = session.query(User).filter(User.email == email).first()
 
 
