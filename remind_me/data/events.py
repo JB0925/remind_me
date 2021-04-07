2f1a7533249c6c0a708cc43da228638a4eddd9e2
Jesse Brink <jbrink0925@gmail.com>
2021-04-07 00:41:43 -0400

    Added psycopg2-binary to lock and toml files, created a db and a users table, created some db related files.

---
+++ remind_me/data/events.py
@@ -0,0 +1,3 @@
+class Events:
+    def __init__(self) -> None:
+        pass
--- remind_me/data/user.py
+++ remind_me/data/user.py
@@ -1,9 +1,19 @@
 from datetime import datetime
 
+import sqlalchemy as sa
 
-class User:
-    def __init__(self, name, email, hashed_password) -> None:
-        self.id = 1
-        self.name = name
-        self.email = email
-        self.hashed_password = hashed_password
+from remind_me.data.modelbase import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String, index=True, unique=True)
+    email = sa.Column(sa.String, index=True, unique=True)
+    hashed_password = sa.Column(sa.String)
+    # def __init__(self, name, email, hashed_password) -> None:
+    #     self.id = 1
+    #     self.name = name
+    #     self.email = email
+    #     self.hashed_password = hashed_password
