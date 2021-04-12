6c843fef17c8c1fc4629cda9cb574b5def309150
Jesse Brink <jbrink0925@gmail.com>
2021-04-11 22:25:40 -0400

    more fixes

--- remind_me/schedule_jobs.py
+++ remind_me/schedule_jobs.py
@@ -1,10 +1,10 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from dateutil.parser import parse
 
 from remind_me.sms import send
 from remind_me.check_email import ReadEmail
 
-sched = BackgroundScheduler()
+sched = BlockingScheduler()
 
 
 def scheduled_job(msg, number, carrier):
