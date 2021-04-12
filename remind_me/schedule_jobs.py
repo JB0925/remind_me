616b06e52536b516333095272a6701fcf5a2e9f0
Jesse Brink <jbrink0925@gmail.com>
2021-04-12 07:14:17 -0400

    more fixes

--- remind_me/schedule_jobs.py
+++ remind_me/schedule_jobs.py
@@ -1,10 +1,10 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from dateutil.parser import parse
 
 from remind_me.sms import send
 from remind_me.check_email import ReadEmail
 
-sched = BlockingScheduler({'apscheduler.timezone': 'EST'})
+sched = BackgroundScheduler({'apscheduler.timezone': 'EST'})
 
 
 def scheduled_job(msg, number, carrier):
@@ -27,7 +27,7 @@ def run_jobs(jobs):
             sched.add_job(scheduled_job, args=(msg,number,carrier), trigger='date', run_date=run_date)
 
 
-def main(jobs):
+def main(jobs=None):
     run_jobs(jobs)
     print(sched.get_jobs())
     try:
@@ -37,6 +37,6 @@ def main(jobs):
 
 
 if __name__ == '__main__':
-    main(jobs)
+    main()
 
 
