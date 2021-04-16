128a137bc54208fd54db9666d9eb8d77a410cd80
Jesse Brink <jbrink0925@gmail.com>
2021-04-15 21:45:03 -0400

    added a jobstore to the Scheduler

--- remind_me/schedule_jobs.py
+++ remind_me/schedule_jobs.py
@@ -1,11 +1,16 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from dateutil.parser import parse
+from uuid import uuid4
 from datetime import timedelta
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from dateutil.parser import parse
+from decouple import config
 
 from remind_me.sms import send
 from remind_me.check_email import ReadEmail
 
-sched = BackgroundScheduler()
+jobstore = {'default': SQLAlchemyJobStore(url=config('DATABASE_URL'))}
+sched = BackgroundScheduler(jobstores=jobstore)
 
 timezones = {
     'EST': 5, 'EDT': 4,
@@ -35,7 +40,8 @@ def run_jobs(jobs, timezone):
         else:
             msg, number, carrier, run_date = job
             run_date = parse(run_date) + + timedelta(hours=timezones[timezone])
-            sched.add_job(scheduled_job, args=(msg,number,carrier), trigger='date', run_date=run_date)
+            sched.add_job(scheduled_job, args=(msg,number,carrier), trigger='date', run_date=run_date,
+                            id=str(uuid4()))
 
 
 def main(jobs, timezone):
