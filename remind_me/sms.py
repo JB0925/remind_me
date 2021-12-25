f6328c45a04814dfd1d88df187f690a4d196b8d2
Jesse Brink <jbrink0925@gmail.com>
2021-12-24 23:06:10 -0500

    updated credentials

--- remind_me/sms.py
+++ remind_me/sms.py
@@ -24,7 +24,7 @@ def send(message, number, carrier):
         raise InvalidCarrier(f"'{carrier}' is not a valid carrier.")
     to_number = str(number)+'@{}'.format(carriers[carrier.lower().strip()])
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server = smtplib.SMTP('smtp.mail.yahoo.com', 465)
     server.ehlo()
     server.starttls()
     server.login(EMAIL, PASSWORD)
